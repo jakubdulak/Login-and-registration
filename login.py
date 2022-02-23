fc8369904aaa8a17ceb30b69f176c3922057a9fb
Jakub Dulak <80412799+jakubdulak@users.noreply.github.com>
2022-02-23 20:52:30 +0100

    login.py
    Prosta aplikacja do logowania i rejestracji. 
    Porównuje dane wprowadzone przez użytkownika z danymi w słowniku lub pozwala na wprowadzenie nowych danych.

---
+++ login.py
@@ -0,0 +1,48 @@
+
+users = {"username": "admin", "password": "admin1"}
+
+password = users.get("password")
+username = users.get("username")
+
+def panel():
+  login = input("Podaj login: ")
+  haslo = input("Podaj hasło: ")
+  choose == '1'
+  logowanie()
+  return
+
+def logowanie(): 
+  if  login == users["username"] and haslo == users["password"]:
+    print("Zalogowano pomyślnie\n")
+    print("Oto tajne informacje...\n")
+  else:
+    if password != haslo:
+      print("Hasło nierawidłowe !")
+      panel()
+    if username != login:
+      print("Login nieprawidłowy !")
+      panel()
+      return
+
+def rejestrowanie():
+  users['username'] = login
+  users['password'] = haslo
+  print("Rejestracja przebiegła pomyślnie\n")
+  print("Teraz możesz się zalogować\n")
+  panel()
+  return
+
+print("Aby uzyskać dostęp zaloguj się lub zarejestruj: \n")
+login = input("Podaj login: ")
+haslo = input("Podaj hasło: ")
+
+print("Wybierz czynność wprowadzając '1' lub '2' \n")
+choose = input("1. Zaloguj się\n"
+    "2. Zarejestruj się")
+
+if choose == '1':
+  print("Logowanie..\n")
+  logowanie()
+elif choose == '2':
+  print("Rejestrowanie..\n")
+  rejestrowanie()
